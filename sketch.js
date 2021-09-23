49ab6b75b5f739db3ae6b8d10980cb87d5403fa7
chriskilinc <chriskilinc@gmail.com>
2021-09-24 01:05:47 +0200

    Init - flowfield

---
+++ sketch.js
@@ -0,0 +1,67 @@
+const points = [];
+const limit = 500;
+let mult = 0.01;
+
+let r1;
+let r2;
+let g1;
+let g2;
+let b1;
+let b2;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(30);
+  angleMode(DEGREES);
+  noiseDetail(1);
+
+  let density = 75;
+  let space = width / density;
+
+  for (let x = 0; x < width; x += space) {
+    for (let y = 0; y < innerHeight; y += space) {
+      let p = createVector(x + random(-10, 10), y + random(-10, 10));
+      points.push(p);
+    }
+  }
+
+  r1 = random(255);
+  r2 = random(255);
+  g1 = random(255);
+  g2 = random(255);
+  b1 = random(255);
+  b2 = random(255);
+
+  mult = random(0.002, 0.01);
+
+}
+
+function draw() {
+  noStroke();
+  let max = points.length;
+  for (let i = 0; i < max; i++) {
+
+    let r = map(points[i].x, 0, width, r1, r2);
+    let g = map(points[i].y, 0, height, g1, g2);
+    let b = map(points[i].x, 0, width, b1, b2);
+    let alpha = map(dist(width / 2, height / 2, points[i].x, points[i].y), 0, limit, limit * 2, 0);
+
+    fill(r, g, b, alpha);
+
+    let angle = map(noise(points[i].x * mult, points[i].y * mult), 0, 1, 0, 720);
+    points[i].add(createVector(cos(angle), sin(angle)));
+
+    if (dist(width / 2, height / 2, points[i].x, points[i].y) < limit)
+      ellipse(points[i].x, points[i].y, 1);
+  }
+}
+
+function keyPressed(e) {
+  console.log(e)
+  if (e.code == "Enter") {
+    saveCanvas("flowfield", "png");
+  }
+}
+
+// CREDITS
+// https://www.youtube.com/watch?v=1-QXuR-XX_s
